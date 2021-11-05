71f9b02023d699f66a843dfcddbd33de74d7dae3
Ian Davis <nospam@iandavis.com>
2021-11-05 11:49:28 +0000

    Wait at least one interval of time when waiting for jobs to complete

--- wait.go
+++ wait.go
@@ -6,14 +6,6 @@ import (
 )
 
 func WaitUntil(ctx context.Context, condition func(context.Context) (bool, error), interval time.Duration) error {
-	done, err := condition(ctx)
-	if err != nil {
-		return err
-	}
-	if done {
-		return nil
-	}
-
 	tick := time.NewTicker(interval)
 	defer tick.Stop()
 
