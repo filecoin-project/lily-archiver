ee480c9c9d47337393b0a1c0e0e56a1f6ffe6472
Ian Davis <nospam@iandavis.com>
2022-03-18 14:20:44 +0000

    Add ohsnap upgrade to default upgrade schedule

--- config.go
+++ config.go
@@ -67,7 +67,7 @@ var (
 			Name:        "upgrade-schedule",
 			EnvVars:     []string{"ARCHIVER_UPGRADE_SCHEDULE"},
 			Usage:       "Network version upgrade schedule for the network. Use a comma separated list of version:height entries. Only upgrades that affect Lily model versions need be included.",
-			Value:       "14:1231620", // mainnet by default
+			Value:       "14:1231620,15:1594680", // mainnet by default
 			Hidden:      true,
 			Destination: &networkConfig.upgradeSchedule,
 		},
